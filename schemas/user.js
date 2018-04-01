96813e4eb2e014e058aacf124a550277e7343227
yangshoxin <15001393240@163.com>
2018-04-01 16:37:00 +0800

    test Object.defineProperty

--- schemas/user.js
+++ schemas/user.js
@@ -5,9 +5,19 @@ var Schema = mongoose.Schema;
 var user = Schema({
     username : 'string',
     password : 'string',
+    num:{
+        type: Number,
+        get: function (t) {
+            return Math.round(t);
+        }
+    },
     posts : [{type: Schema.Types.ObjectId, ref: 'Arctic'}]
 });
 
+var Test = mongoose.model('Test',user);
+var doc = new Test();
+doc.num = 2.001;
+console.log(doc.num);
 module.exports = user;
 
 
